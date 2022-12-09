a571f73021b8ed4f8cb679a82c63d215d3c20ab8
JoseAcevedo6 <acevedo0803@gmail.com>
2022-12-09 13:00:23 -0300

    entrega

--- fast_api/main.py
+++ fast_api/main.py
@@ -2,8 +2,8 @@ from fastapi import FastAPI
 from connect import connect_execute, query1, query2, query3, query4
 
 app = FastAPI()
+print(connect_execute(query4('Amazon', 2020)))
 
 @app.get("/")
 def read_root():
-    respuesta = connect_execute(query4('Amazon', 2020))
-    return respuesta
+    return connect_execute(query4('Amazon', 2020))

18f9945b0371b79e149649ebbdec30716fe4a46d
JoseAcevedo6 <acevedo0803@gmail.com>
2023-04-03 02:08:28 -0300

    main.py updated

---
+++ fast_api/etl_data_creator.py
@@ -0,0 +1,180 @@
+from pandas import DataFrame, concat, read_csv, read_json
+
+from database import Engine
+
+
+def cleansing(df, column):
+
+    dicc = {'["#]': '', r"[-'\._()]": " ", '4k': '', 'uhd': '', '&': 'and'}
+    dicc_listed_in = {
+        '["#/+&,]': '', r"['\._()]": " ", 'documentaries|docuseries': 'documentary', 'stand.+up': 'stand-up',
+        'shows|show|series|movies|film|tv|videos|features|young|audience|musicals|and': '', 'thrillers': 'thriller',
+        'concerts': 'concert', 'classics': 'classic', 'action-adventure': 'action adventure', 'historical': 'history',
+        'romantic': 'romance', 'dramas': 'drama', 'musical': 'music', 'comedies': 'comedy', 'mysteries': 'mystery',
+        'military.+war': 'military-war', 'health.+wellness': 'health-wellness', 'science.+fiction': 'sci-fi',
+        'black.+stories': 'black-stories', 'late.+night': 'late-night', 'faith.*spirituality': 'faith-spirituality',
+        'coming.*of.*age': 'coming-of-age', 'special.+interest': 'special-interest', 'cooking.*food': 'cooking-food',
+        'soap.*opera.*melodrama': 'soap-opera-melodrama',
+        }
+
+    dicc = dicc_listed_in if column == 'listed_in' else dicc
+
+    df[column] = df[column].str.lower()
+    df[column].replace(dicc, regex=True, inplace=True)
+    df[column].replace({'  ': ' ', '   ': ' ', 'st-up': 'stand-up'}, regex=True, inplace=True)
+    df[column] = df[column].str.strip()
+    df[column] = df[column].str.title()
+
+    return df[column]
+
+
+def create_df_actor_listed_in(df, column, sep=' '):
+
+    # Funcion para separar valores de filas de una dataframe[column] y rotornar un nuevo dataframe con dichos valores
+    # en una sola columna. Recibe como primer parametro una columna de un dataframe y como segundo parametro el
+    # elemento por el cual queremos separar (default white space).
+
+    df_aux = df[column].str.split(sep, expand=True)
+
+    # Convertimos el dataframe en un objeto numpy para unificar todo en una columna con el metodo flatten
+    df_aux = df_aux.to_numpy().flatten()
+
+    # Convertimos nuevamente a dataframe, eliminamos valores nulos, espacios en blanco si procede, valores repetidos
+    # y ordenamos
+    df_aux = DataFrame(df_aux)
+    df_aux[0] = df_aux[0].str.strip()
+
+    df_aux.replace({'': None, 'sin dato': None}, inplace=True)
+    df_aux.dropna(inplace=True)
+    df_aux.drop_duplicates(inplace=True)
+    df_aux.drop(df_aux[df_aux[0].str.len() == 1].index, inplace=True)
+    df_aux.sort_values(0, inplace=True)
+    df_aux.reset_index(drop=True, inplace=True)
+    df_aux.index += 1
+    df_aux.reset_index(drop=False, inplace=True)
+
+    column_name = 'actor' if column == 'cast' else column
+    df_aux.columns = [f'id_{column_name}', column_name]
+
+    return df_aux
+
+
+def split_rows_df(df, column, split):
+
+    df_aux = df[df[column] == split].copy()
+
+    df_aux['duration'].replace({'sin dato': None}, regex=False, inplace=True)
+    df_aux['duration'] = df_aux['duration'].astype('Int64')
+    df_aux['duration'].fillna(int(df_aux[['duration']].mean(skipna=True)), inplace=True)
+    df_aux['duration'].replace({0: 1}, regex=False, inplace=True)
+
+    df_aux = df_aux[['title_name', 'id_service', 'cast', 'release_year', 'duration', 'listed_in']]
+
+    return df_aux
+
+
+def create_df_aux(df, col1, col2, sep):
+
+    df = df[[col1, col2]]
+    df.set_index(col1, inplace=True)
+    df = df[col2].str.split(sep, expand=True)
+
+    df_aux = DataFrame()
+
+    for i in range(len(df.columns)):
+        df_aux = concat([df_aux, df[i]], axis=0)
+
+    df_aux = df_aux[0].str.strip()
+    df_aux.replace({'': None, 'sin dato': None}, inplace=True)
+    df_aux.dropna(inplace=True)
+    df_aux = df_aux.reset_index()
+    df_aux.drop(df_aux[df_aux[0].str.len() == 1].index, inplace=True)
+    df_aux.columns = [col1, 'actor' if col2 == 'cast' else col2]
+    df_aux = df_aux.sort_values(col1)
+    df_aux.drop_duplicates(inplace=True)
+
+    return df_aux
+
+
+df_amazon = read_csv("datasets/amazon_prime_titles.csv")
+df_amazon['id_service'] = 1
+df_disney = read_csv("datasets/disney_plus_titles.csv")
+df_disney['id_service'] = 2
+df_hulu = read_csv("datasets/hulu_titles.csv")
+df_hulu['id_service'] = 3
+df_netflix = read_json("datasets/netflix_titles.json")
+df_netflix['id_service'] = 4
+
+df_concated = concat([df_amazon, df_disney, df_hulu, df_netflix], axis=0, ignore_index=True)
+df_concated['duration'] = df_concated['duration'].str.split(expand=True)[0]
+df_concated.rename({'title': 'title_name'}, axis=1, inplace=True)
+df_concated.fillna('sin dato', inplace=True)
+
+df_concated['title_name'] = cleansing(df_concated, 'title_name')
+df_concated['cast'] = cleansing(df_concated, 'cast')
+df_concated['listed_in'] = cleansing(df_concated, 'listed_in')
+
+df_concated.sort_values('title_name', inplace=True)
+df_concated.reset_index(drop=True, inplace=True)
+
+df_actor = create_df_actor_listed_in(df_concated, 'cast', ',')
+
+df_listed_in = create_df_actor_listed_in(df_concated, 'listed_in')
+
+df_service = DataFrame({'id_service': [1, 2, 3, 4], 'service_name': ['Amazon', 'Disney', 'Hulu', 'Netflix']})
+
+df_title = df_concated[['title_name']]
+df_title = df_title.drop_duplicates()
+df_title.reset_index(drop=True, inplace=True)
+df_title.index += 1
+df_title = df_title.rename_axis('id_title').reset_index()
+
+df_movie = split_rows_df(df_concated, 'type', 'Movie')
+df_movie = df_movie.merge(df_title, how='left', on='title_name')
+
+df_serie = split_rows_df(df_concated, 'type', 'TV Show')
+df_serie = df_serie.merge(df_title, how='left', on='title_name')
+
+df_movie_title_service = df_movie[['id_title', 'id_service']].copy()
+df_movie_title_service.drop_duplicates(inplace=True)
+
+df_serie_title_service = df_serie[['id_title', 'id_service']].copy()
+df_serie_title_service.drop_duplicates(inplace=True)
+
+df_movie_title_actor = create_df_aux(df_movie, 'id_title', 'cast', ',')
+df_movie_title_actor = df_movie_title_actor.merge(df_actor, how='left', on='actor')
+df_movie_title_actor.drop(['actor'], axis=1, inplace=True)
+
+df_serie_title_actor = create_df_aux(df_serie, 'id_title', 'cast', ',')
+df_serie_title_actor = df_serie_title_actor.merge(df_actor, how='left', on='actor')
+df_serie_title_actor.drop(['actor'], axis=1, inplace=True)
+
+df_movie_title_listed_in = create_df_aux(df_movie, 'id_title', 'listed_in', ' ')
+df_movie_title_listed_in = df_movie_title_listed_in.merge(df_listed_in, how='left', on='listed_in')
+df_movie_title_listed_in.drop(['listed_in'], axis=1, inplace=True)
+
+df_serie_title_listed_in = create_df_aux(df_serie, 'id_title', 'listed_in', ' ')
+df_serie_title_listed_in = df_serie_title_listed_in.merge(df_listed_in, how='left', on='listed_in')
+df_serie_title_listed_in.drop(['listed_in'], axis=1, inplace=True)
+
+df_movie = df_movie[['id_title', 'release_year', 'duration']]
+df_movie.sort_values(by=['duration', 'id_title'], inplace=True)
+df_movie.drop_duplicates(subset=['id_title'], keep='last', inplace=True)
+
+df_serie = df_serie[['id_title', 'release_year', 'duration']]
+df_serie.sort_values(by=['duration', 'id_title'], ascending=False, inplace=True)
+df_serie.drop_duplicates(subset=['id_title'], keep='first', inplace=True)
+
+
+df_actor.to_sql('actor', Engine, if_exists='append', index=False)
+df_listed_in.to_sql('listed_in', Engine, if_exists='append', index=False)
+df_movie.to_sql('movie', Engine, if_exists='append', index=False)
+df_movie_title_actor.to_sql('movie_title_actor', Engine, if_exists='append', index=False)
+df_movie_title_listed_in.to_sql('movie_title_listed_in', Engine, if_exists='append', index=False)
+df_movie_title_service.to_sql('movie_title_service', Engine, if_exists='append', index=False)
+df_serie.to_sql('serie', Engine, if_exists='append', index=False)
+df_serie_title_actor.to_sql('serie_title_actor', Engine, if_exists='append', index=False)
+df_serie_title_listed_in.to_sql('serie_title_listed_in', Engine, if_exists='append', index=False)
+df_serie_title_service.to_sql('serie_title_service', Engine, if_exists='append', index=False)
+df_service.to_sql('service', Engine, if_exists='append', index=False)
+df_title.to_sql('title', Engine, if_exists='append', index=False)
--- fast_api/main.py
+++ fast_api/main.py
@@ -1,8 +1,126 @@
-from fastapi import FastAPI
-from connect import connect_execute, query1, query2, query3, query4
+from fastapi import FastAPI, status, responses, encoders
+import uvicorn
+
+from database import Func, Column, Session, Desc
+from models import Actor, Listed_in, Movie, Serie, Service, Title, Movie_title_actor, Movie_title_listed_in,\
+    Movie_title_service, Serie_title_actor, Serie_title_listed_in, Serie_title_service
+
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return connect_execute(query4('Amazon', 2020))
+
+@app.get("/get_max_duration")
+def get_max_duration(year: int, platform: str, type: str):
+
+    db = Session()
+
+    if type.lower() == 'min':
+        query = db.query(
+            Title.title_name, Movie.release_year, Func.max(Movie.duration), Movie.duration,
+            Column("Min").label('duration_unit'), Service.service_name)\
+            .select_from(Title).join(Movie).join(Movie_title_service).join(Service)\
+            .where(Movie.release_year == year).where(Service.service_name == platform.capitalize()).all()
+    if type.lower() == 'season':
+        query = db.query(
+            Title.title_name, Serie.release_year, Func.max(Serie.duration), Movie.duration,
+            Column("Season").label('duration_unit'), Service.service_name)\
+            .select_from(Title).join(Serie).join(Serie_title_service).join(Service)\
+            .where(Serie.release_year == year).where(Service.service_name == platform.capitalize()).all()
+
+    result_dicts = [r._asdict() for r in query]
+
+    db.close()
+
+    return responses.JSONResponse(status_code=status.HTTP_200_OK, content=encoders.jsonable_encoder(result_dicts))
+
+
+@app.get("/get_count_platform")
+def get_count_platform(platform: str):
+
+    db = Session()
+
+    movie_query = db.query(
+        Column("Movie").label('type'), Func.count(Title.title_name).label('cantidad'), Service.service_name)\
+        .select_from(Title).join(Movie_title_service)\
+        .join(Service).where(Service.service_name == platform.capitalize())
+
+    serie_query = db.query(
+        Column("Serie").label('type'), Func.count(Title.title_name).label('cantidad'), Service.service_name)\
+        .select_from(Title).join(Serie_title_service).\
+        join(Service).where(Service.service_name == platform.capitalize())
+
+    query = movie_query.union(serie_query).all()
+    result_dicts = [r._asdict() for r in query]
+
+    db.close()
+
+    return responses.JSONResponse(status_code=status.HTTP_200_OK, content=encoders.jsonable_encoder(result_dicts))
+
+
+@app.get("/get_listedin")
+def get_listedin(listed_in: str):
+
+    db = Session()
+
+    query1 = db.query(Movie_title_listed_in.id_title, Listed_in.listed_in, Service.service_name)\
+        .select_from(Movie_title_listed_in)\
+        .join(Movie_title_service, Movie_title_service.id_title == Movie_title_listed_in.id_title)\
+        .join(Service, Service.id_service == Movie_title_service.id_service)\
+        .join(Listed_in, Listed_in.id_listed_in == Movie_title_listed_in.id_listed_in)\
+        .where(Listed_in.listed_in == listed_in.capitalize())
+
+    query2 = db.query(Serie_title_listed_in.id_title, Listed_in.listed_in, Service.service_name)\
+        .select_from(Serie_title_listed_in)\
+        .join(Serie_title_service, Serie_title_service.id_title == Serie_title_listed_in.id_title)\
+        .join(Service, Service.id_service == Serie_title_service.id_service)\
+        .join(Listed_in, Listed_in.id_listed_in == Serie_title_listed_in.id_listed_in)\
+        .where(Listed_in.listed_in == listed_in.capitalize())
+
+    gen_query = query1.union_all(query2)
+    count_listed_in = gen_query.with_entities(Listed_in.listed_in, Func.count().label('count'), Service.service_name)\
+        .group_by(Service.service_name).order_by(Desc(Func.count()))
+    count_listed_in = count_listed_in.first()._asdict() if count_listed_in else None
+
+    db.close()
+
+    return responses.JSONResponse(status_code=status.HTTP_200_OK, content=encoders.jsonable_encoder(count_listed_in))
+
+
+@app.get("/get_actor")
+def get_actor(platform: str, release_year: int):
+
+    db = Session()
+
+    query1 = db.query(Actor.actor, Movie.release_year, Service.service_name)\
+        .select_from(Movie)\
+        .join(Movie_title_service, Movie_title_service.id_title == Movie.id_title)\
+        .join(Service, Service.id_service == Movie_title_service.id_service)\
+        .join(Movie_title_actor, Movie_title_actor.id_title == Movie.id_title)\
+        .join(Actor, Actor.id_actor == Movie_title_actor.id_actor)\
+        .where(Service.service_name == platform.capitalize()).where(Movie.release_year == release_year)
+
+    query2 = db.query(Actor.actor, Serie.release_year, Service.service_name)\
+        .select_from(Serie)\
+        .join(Serie_title_service, Serie_title_service.id_title == Serie.id_title)\
+        .join(Service, Service.id_service == Serie_title_service.id_service)\
+        .join(Serie_title_actor, Serie_title_actor.id_title == Serie.id_title)\
+        .join(Actor, Actor.id_actor == Serie_title_actor.id_actor)\
+        .where(Service.service_name == platform.capitalize()).where(Serie.release_year == release_year)
+
+    actor_count = query1.union_all(query2)
+    actor_count = actor_count.with_entities(
+        Actor.actor, Func.count(Actor.actor).label('count'), Serie.release_year, Service.service_name)\
+        .group_by(Actor.actor).order_by(Desc(Func.count(Actor.actor)))
+
+    if 'Sin Dato' in actor_count.first():
+        actor_count = actor_count.offset(1).first()._asdict()
+    else:
+        actor_count = actor_count.first()._asdict()
+
+    db.close()
+
+    return responses.JSONResponse(status_code=status.HTTP_200_OK, content=encoders.jsonable_encoder(actor_count))
+
+
+if __name__ == '__main__':
+    uvicorn.run('endpoints:app', host='127.0.0.1', port=8080, reload=True)
